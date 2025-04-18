af3b277d68e50227ba92cd10bd2e74d0829dec0e
dixiq-404 <papalam72@yandex.ru>
2025-04-18 20:22:09 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -64,7 +64,7 @@ function check_download {
 
     if [ ! -d "${K_SHAKETUNE_PATH}" ]; then
         echo "[DOWNLOAD] Downloading Klippain Shake&Tune module repository..."
-        if git -C $shaketunedirname clone https://github.com/dixiq-404/klippain-shaketune-for-qidi-plus4.git $shaketunebasename; then
+        if git -C $shaketunedirname clone https://github.com/dixiq-404/q1_shake.git $shaketunebasename; then
             chmod +x ${K_SHAKETUNE_PATH}/install.sh
             printf "[DOWNLOAD] Download complete!\n\n"
         else
